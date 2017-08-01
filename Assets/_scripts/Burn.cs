ca691ea60074c64a6a180dcb3ada3fa7e7d624df
euworld1 <euworld1@gmail.com>
2017-08-01 17:12:14 -0600

    Functional Burner with fire

--- Assets/_scripts/Burn.cs
+++ Assets/_scripts/Burn.cs
@@ -6,20 +6,18 @@ public class Burn : MonoBehaviour
 {
 
 	public Transform Fire;
-	private ParticleSystem ps;
-	public bool moduleEnabled;
-
+	public GameObject Prefab;
 	// Use this for initialization
 	void Start () 
 	{
 		//Fire.GetComponent<ParticleSystem>().enableEmission = true;
-		ps = GetComponent<ParticleSystem>();
+		//ps = GetComponent<ParticleSystem>();
 
 	}
 	void Update() 
 	{
-		var emmision = ps.emission;
-		emmision.enabled = moduleEnabled;
+		//var emmision = ps.emission;
+		//emmision.enabled = moduleEnabled;
 	}
 
 //	void OnGUI() 
@@ -27,17 +25,23 @@ public class Burn : MonoBehaviour
 //		moduleEnabled = GUI.Toggle (new Rect (25, 45, 100, 30), moduleEnabled, "Enabled");
 //	}
 	
-	void OnTriggerEnter(Collider burner)
+	void OnTriggerEnter()
 	{
 		
 		Debug.Log ("Hand is touching the burner start");
 		//Fire.GetComponent<ParticleSystem> ().enableEmission = true;
 		//burner.rigidbody.GetComponent<ParticleSystem> ().enableEmission;
-		ParticleSystem ps = GetComponent<ParticleSystem>();
-
-
+		//ParticleSystem ps = GetComponent<ParticleSystem>();
+		//Destroy (burner.gameObject);	
+					
+		Instantiate (Prefab, Fire.position, Fire.rotation);
+			
 
 	}
+
+
+
+
 	void OnTriggerStay(Collider burner)
 	{
 		Debug.Log ("Hand is touching the burner");
