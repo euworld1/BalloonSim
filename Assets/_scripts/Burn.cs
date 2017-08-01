d9b04214914078cefb082f9f6453ff66dc8c1837
euworld1 <euworld1@gmail.com>
2017-07-31 18:08:54 -0600

    added Touch Controller

---
+++ Assets/_scripts/Burn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burn : MonoBehaviour {
+
+	public Transform Fire;
+
+	// Use this for initialization
+	void Start () {
+		Fire.GetComponent<ParticleSystem> ().enableEmission = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		ContactPoint contact = collision.contacts[0];
+		Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+		Vector3 pos = contact.point;
+		Instantiate(Fire, pos, rot);
+		Destroy(Fire);
+	}
+
+}
---
+++ Assets/_scripts/TouchController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = OVRInput.GetLocalControllerPosition (OVRInput.Controller.RTouch);
+
+	}
+}
